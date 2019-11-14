2c9e1b7e266ce60b245e3bfa796f252de2870395
Hamed Behjat <hamed.behjat@gmail.com>
2019-11-14 14:49:10 +0330

    testing convertor

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -10,30 +10,56 @@ class Converter extends Component {
     };
   }
 
+  syncData = (event) => {
+    // console.log(typeof(event.target.name));
+    if( event.target.name === "dolar"){
+      
+        console.log(event.target.value);
+        let temp = event.target.value * 13000;
+        console.log(temp);
+        this.setState({toman : temp,dollar:event.target.value});
+    }else{
+     
+      console.log(event.target.name);
+     
+      
+      let temp = parseInt(event.target.value) /13000; 
+      console.log(temp);
+      this.setState({toman : temp.toString(),dollar :event.target.value.toString() });
+    }
+  };
+
   render() {
     return (
       <div className="container">
         <form>
-          <div class="form-row">
+          <div className="form-row">
             <div className="col-6">
               <input
                 type="text"
+                placeholder="dolar"
+                name="dolar"
                 className="form-control"
-                value={this.state.toman}
-                onChange=""
+                // value={this.state.toman}
+                onChange={this.syncData}
               ></input>
             </div>
             <div className="col-6">
               <input
                 type="text"
+                placeholder="toman"
+                name="toman"
                 className="form-control"
                 value={this.state.dollar}
-                onChange=""
+                onChange={this.syncData}
+                // onChange="{this.syncData}"
               ></input>
             </div>
           </div>
         </form>
       </div>
+      // <input type="text" onChange={(e)=>{console.log(e.target.value);
+      // }}/>
     );
   }
 }
